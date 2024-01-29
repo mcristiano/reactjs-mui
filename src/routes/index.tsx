97d21ee63212059bb72902f61288258b6923c747
mcristiano <machado.cristiano@gmail.com>
2024-01-29 02:52:39 +0000

    modified:   package-lock.json 	modified:   package.json 	modified:   src/routes/index.tsx 	modified:   yarn.lock

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,12 @@
+import { Button } from '@mui/material';
 import { Routes, Route, Navigate } from 'react-router-dom';
-
-
 
 export const AppRoutes = () => {
   const PAGE_INITIAL = '/pagina-inicial';
 
   return (
     <Routes>
-      <Route path={PAGE_INITIAL} element={<p>Pagina Inicial</p>} />
+      <Route path={PAGE_INITIAL} element={<Button>Teste</Button>} />
 
       <Route path="*" element={<Navigate to={PAGE_INITIAL} />} />
     </Routes>
